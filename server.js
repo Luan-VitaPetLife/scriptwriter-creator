8efeb0f7697ea382c898786492ab3ff09423e4b8
Luan <luanmazzi305@gmail.com>
2026-06-02 15:36:59 -0300

    Ajustes finais - Antes de separar o código

--- server.js
+++ server.js
@@ -506,5 +506,5 @@ module.exports = app;
 
 if (process.env.NODE_ENV !== 'production') {
   const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => console.log(`🚀 Sistema Online na porta ${PORT}`));
+  app.listen(PORT, () => console.log(`Sistema Online na porta ${PORT}`));
 }
